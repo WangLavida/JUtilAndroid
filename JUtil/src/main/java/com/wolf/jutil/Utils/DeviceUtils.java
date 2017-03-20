d8e41074c10c2392f883d4fdbe0cb7fa7257b1b0
WangLavida <0706wolf@sina.com>
2017-03-20 14:13:06 +0800

    将px值转换为sp值
    将px值转换为sp值

--- JUtil/src/main/java/com/wolf/jutil/Utils/DeviceUtils.java
+++ JUtil/src/main/java/com/wolf/jutil/Utils/DeviceUtils.java
@@ -40,4 +40,11 @@ public class DeviceUtils {
         final float scale = context.getResources().getDisplayMetrics().density;
         return (int) (pxValue / scale + 0.5f);
     }
+  /**
+     * 将px值转换为sp值，保证文字大小不变
+     */
+    public static int px2sp(Context context, float pxValue) {
+        final float fontScale = context.getResources().getDisplayMetrics().scaledDensity;
+        return (int) (pxValue / fontScale + 0.5f);
+    }
 }
